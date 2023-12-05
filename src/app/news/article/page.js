7ebd83f865e854a38cd0b0aa666672b60320c1a7
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-12-06 04:47:34 +0600

    created nested route blog and article page

---
+++ src/app/news/article/page.js
@@ -0,0 +1,9 @@
+const ArticlePage = () => {
+  return (
+    <div>
+      <h2>This is article page.</h2>
+    </div>
+  );
+};
+
+export default ArticlePage;
---
+++ src/app/news/blog/page.js
@@ -0,0 +1,9 @@
+const BlogPage = () => {
+  return (
+    <div>
+      <h2>This is blog page</h2>
+    </div>
+  );
+};
+
+export default BlogPage;
